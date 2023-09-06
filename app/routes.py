0a658cc14d69cfa6603b84dfe801c9854022e7e1
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-09-06 12:57:12 +0100

    Working on a fix for the importError in the music recommender collaborative project

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, redirect, url_for, session, request
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import spotipy.util as sp_util  # Import Spotify utility functions
+import spotipy.util as sp_util
 import os
+from app import create_app  # Import create_app function
+
+app = create_app()  # Initialize the Flask app
 
 # Define Spotify API credentials and settings
 client_id = os.environ.get('SPOTIPY_CLIENT_ID')
@@ -10,8 +13,8 @@ client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
 redirect_uri = 'http://localhost:5000/callback'
 scope = 'user-library-read user-top-read'
 
-# Set this to True for testing but you probably want it set to False in production.
-SHOW_DIALOG = True
+# Set this to False for production
+SHOW_DIALOG = False
 
 @app.route('/')
 def home():
@@ -59,7 +62,7 @@ def recommendations():
         tracks_by_genre = sp.search(q=f'genre:"{genre}"', type='track', limit=5)
         recommended_tracks.extend(tracks_by_genre['tracks']['items'])
 
-    # For simplicity, let's just pass the recommended tracks to the recommendations template
+    # Pass both top_tracks and recommended_tracks to the recommendations template
     return render_template('recommendations.html', recommended_tracks=recommended_tracks, top_tracks=top_tracks)
 
 def create_spotify_oauth():
@@ -68,7 +71,7 @@ def create_spotify_oauth():
         client_secret=client_secret,
         redirect_uri=redirect_uri,
         scope=scope
-        )
+    )
 
 def refresh_token_if_expired():
     token_info = session['token_info']
@@ -80,7 +83,7 @@ def refresh_token_if_expired():
                 client_id=sp_oauth.client_id,
                 client_secret=sp_oauth.client_secret,
                 redirect_uri=sp_oauth.redirect_uri,
-                cache_path=None # Disable caching for simplicity
+                cache_path=None  # Disable caching for simplicity
                 )
         token_info = sp_oauth.refresh_access_token(session['token_info']['refresh_token'])
         session['token_info'] = token_info
